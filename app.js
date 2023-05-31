89eba677a1c9dcea744a63e8d49ca9fbc86cc56e
Makson <nooplace55@gmail.com>
2023-05-31 22:45:13 +0600

    added sticky nav bar

--- app.js
+++ app.js
@@ -238,3 +238,8 @@
 //     else {temp.className = "positive";}
 //     }
 //     };
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header")
+    header.classList.toggle("sticky", window.scrollY > 140)
+})
